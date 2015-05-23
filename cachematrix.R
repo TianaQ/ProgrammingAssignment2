5eab5ba64071745161ea3f341e40754ad416c6fb
Tatiana <kurilo.tatiana@gmail.com>
2015-05-24 00:05:59 +0300

    My new version of cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 of R Programming course on Coursera.org 
+## by Tatiana Kurilo
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  
+  check <- function(y) {          ## tests wheather the matrix is invertible
+    if (ncol(y) != nrow(y)) {
+      message: "The matrix must be square"
+      NA
+      return()
+    }
+    
+    if (det(y) == 0) {
+      message: "The matrix must be invertible"
+      NA
+      return()
+    }
+    TRUE
+  }
+  
+  if (is.null(check(x))) {
+    NA
+    return() ## exits function if the matrix is not appropriate
+  }              
+  
+  set <- function(newMatrix) {
+    if (is.null(check(newMatrix))) {
+      NA
+      return()  ## exits function if the matrix is not appropriate
+    }       
+    x <<- newMatrix
+    invMatrix <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setInvMatrix <- function(newInvMatrix){
+    invMatrix <<- newInvMatrix
+  }
+  
+  getInvMatrix <- function(){
+    invMatrix
+  }
+  
+  list(set = set, get = get, setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of a special "matrix" returned by makeCacheMatrix function. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getInvMatrix()
+  
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  ## if there is no cached data:
+  matrix <- x$get()
+  
+  inv <- solve(matrix)
+  
+  x$setInvMatrix(inv)
+  
+  inv   
 }
